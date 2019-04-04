9c90d03f15eca2e99062e05e037394da73325282
Lukasz Krainski <lukasz.krainski@ryerson.ca>
2019-04-04 14:28:38 -0400

    runtest.jl fix #3

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,6 @@ using OpenStreetMapX
 
 @testset "maps" begin
 
-m = OpenStreetMapX.get_map_data("example/reno_east3.osm",use_cache=false);
-
-@test length(m.nodes) == 9032
-
 using Random
 Random.seed!(0);
 @test rand(Int) == -4635026124992869592
