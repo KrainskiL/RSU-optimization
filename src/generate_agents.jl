bd0146df24f5364c453034453c2ad4f68ad563b9
Lukasz Krainski <lukasz.krainski123@gmail.com>
2019-04-08 09:18:15 -0400

    Pick_random_node #2

--- src/generate_agents.jl
+++ src/generate_agents.jl
@@ -8,30 +8,32 @@ Nodes are randomly chosen from set of rectangles corresponding to areas on map.
 
 **Input parameters**
 * `map` : MapData type from OpenStreetMapX package
-* `rects` : vector of tuples with two Latitude-Longitude point interpreted as set of rectangles on map;
-first point - rectangle bottom left corner
-second point - top right corner
-"""
-SetRecs = Vector{Tuple{Tuple{Float64,Float64},Tuple{Float64,Float64}}}
-function pick_random_node(map::MapData, rects::SetRecs)
-#Check bottom left and top right points compliance
-    [if !(rect[1][1] < rect[2][1] || rect[1][2] < rect[2][2])
-    throw(DomainError(rect, "wrong coordinates definition")) end for rect in rects]
-    nodes_in_rects = Vector{Int}()
-    for rect in rects
-        for node in map_data.nodes
-            if (node.ENU.east >= ENU(LLA(rec[1][1]))
+* `rects` : vector of tuples with two Latitude-Longitude point interpreted as a set of rectangle areas;
 
+"""
+Rect = Tuple{Tuple{Float64,Float64},Tuple{Float64,Float64}}
+function pick_random_node(map::MapData, rects::Vector{Rect})
+#Check bottom left and top right points compliance
+    [if (rect[1][1] == rect[2][1] || rect[1][2] == rect[2][2])
+    throw(DomainError(rect, "coordinates not defining a rectangle")) end for rect in rects]
+    nodes_in_rects = Vector{Int}()
+
+    for rect in rects
+        frect = collect(Iterators.flatten(rect))
+        p1 = ENU(LLA(frect[1], frect[2]), map_data.bounds)
+        p2 = ENU(LLA(frect[3], frect[4]), map_data.bounds)
+        exE = extrema([p1.east, p2.east])
+        exN = extrema([p1.north, p2.north])
+        for (key, value) in map_data.nodes
+            if (exE[1] <= value.east <= exE[2] &&
+                exN[1] <= value.north <= exN[2])
+                push!(nodes_in_rects, key)
             end
         end
     end
+    chosen_node = rand(unique!(nodes_in_rects))
     return chosen_node
 end
+v
 
-collect([(5.6346,6.2341),(4.123,5.74343)])
-
-pick_random_node(map_data, [((5.6346,6.2341),(4.123,5.74343))])
-typeof(ans)
-return 1 <=2
-
-map_data.nodes[140002483]
+@time pick_random_node(map_data, [((41.0,-119.70),(39.0,-119.74)),((38.8,-119.60),(39.0,-119.65))])
